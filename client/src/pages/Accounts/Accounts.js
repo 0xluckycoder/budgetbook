b4d3e11e4d012a717fa0d9c2472e7c63cb0f27f6
0xluckycoder <lakshanperera.dev@gmail.com>
2022-09-12 23:39:33 +0530

    Accounts UI completed

--- client/src/pages/Accounts/Accounts.js
+++ client/src/pages/Accounts/Accounts.js
@@ -3,33 +3,39 @@ import { AppLayout } from "../AppLayout";
 import { Col, Row } from 'antd';
 import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
 import { AccountTotalCard } from "../../containers/AccountTotalCard/AccountTotalCard";
+// import { AccountCard } from "../../containers/AccountCard/AccountCard";
+import { AccountCardSelect } from "../../containers/AccountCardSelect/AccountCardSelect";
 import styles from './account.module.scss';
-
-const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
-];
 
 const Accounts = () => {
     return (
         <AppLayout>
             <h2 className={styles.heading}>Accounts</h2>
             <div className={styles.accounts}>
-            <Row gutter={12}>
-                {/* <AccountTotalCard /> */}
+                <Row gutter={12}>
                     <Col lg={12} md={24} sm={24} xs={24} className={`${styles.col} gutter-row`}>
                         <AccountTotalCard />
                     </Col>
                     <Col lg={12} md={24} sm={24} xs={24} className={`${styles.col} gutter-row`}>
-                        {/* <AccountTotalCard /> */}
                     </Col>
-                    {/* <Col lg={8} md={24} sm={24} xs={24} className={`${styles.col} gutter-row`}> */}
-                        {/* <AccountTotalCard /> */}
-                    {/* </Col> */}
                 </Row>
-                {/* <AccountTotalCard /> */}
+
+                <br/>
+
+                <h2 className={styles.heading}>List of Accounts</h2>
+
+                <Row gutter={20}>
+                    <Col lg={8} md={24} sm={24} xs={24} className={`${styles.col} gutter-row`}>
+                        <AccountCardSelect />
+                    </Col>
+                    <Col lg={8} md={24} sm={24} xs={24} className={`${styles.col} gutter-row`}>
+                        <AccountCardSelect />
+                    </Col>
+                    <Col lg={8} md={24} sm={24} xs={24} className={`${styles.col} gutter-row`}>
+                        <AccountCardSelect />
+                    </Col>
+                </Row>
+
             </div>
         </AppLayout>
     );
